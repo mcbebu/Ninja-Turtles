e68ddc5f302e258694398d04af16d0c502d921a1
Chu Yi Ting <ytyt3902@outlook.com>
2023-02-25 19:42:41 +0800

    Change some GUI

--- src/main/java/NinjaChat/MainWindow.java
+++ src/main/java/NinjaChat/MainWindow.java
@@ -22,8 +22,6 @@ public class MainWindow extends AnchorPane {
     private Button sendButton;
     private NinjaBot ninjaBot;
 
-    // userImage taken from "https://www.pngfind.com/mpng/TihxTxw_asta-black-clover-black-clover-asta-png-transparent/"
-    // dukeImage taken from "https://blackcloverthots.tumblr.com/post/612892427599134720/marisashinx-nero-gets-what-nero-deserves"
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
     private Image ninjaImage = new Image(this.getClass().getResourceAsStream("/images/ninjavan.png"));
 
@@ -31,7 +29,7 @@ public class MainWindow extends AnchorPane {
     public void initialize() {
         Ui ui = new Ui();
         dialogContainer.getChildren().addAll(
-                DialogBox.getDukeDialog(ui.printGreetingMessage(), ninjaImage));
+                DialogBox.getNinjaChatDialog(ui.printGreetingMessage(), ninjaImage));
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
     }
 
@@ -50,7 +48,7 @@ public class MainWindow extends AnchorPane {
         String response = ninjaBot.getResponse(input);
         dialogContainer.getChildren().addAll(
                 DialogBox.getUserDialog(input, userImage),
-                DialogBox.getDukeDialog(response, ninjaImage)
+                DialogBox.getNinjaChatDialog(response, ninjaImage)
         );
         userInput.clear();
     }
